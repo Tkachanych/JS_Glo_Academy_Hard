8103a0ca4864ab855ce9464a3e5a6026d71d3c53
Vladimir Tkachenko <geletar@gmail.com>
2023-01-28 12:44:28 +0400

    Второе задание

--- script.js
+++ script.js
@@ -1,23 +1,5 @@
 'use strict';
 
-// Первое задание
-
-let title = 'JS_Glo_Academy';
-let screens = 'Простые, Сложные, Интерактивные';
-let screenPrice = 2500;
-let rollback = 15;
-let fullPrice = 500000;
-let adaptive = true;
-
-console.log(typeof title);
-console.log(typeof fullPrice);
-console.log(typeof adaptive);
-console.log(screens.length);
-console.log(`Стоимость верстки экранов ${screenPrice} рублей`);
-console.log(`Стоимость разработки сайта ${fullPrice} рублей`);
-console.log(screens.toLowerCase().split(', '));
-console.log(fullPrice * (rollback / 100));
-
 // Второе задание
 
 // Создать переменную num со значением 266219 (тип данных число)
@@ -33,4 +15,5 @@ console.log(result);
 //Полученный результат возвести в степень 3, используя только 1 оператор
 result **= 3;
 
-console.log(result);
+//Вывести в консоль первые 2 цифры полученного числа
+console.log(result.toString().slice(0,2));
