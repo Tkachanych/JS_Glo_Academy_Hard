f26d418e3cf6139b2951dd105f637df38d74b3f6
Vladimir Tkachenko <geletar@gmail.com>
2023-01-30 22:58:19 +0400

    lesson03

--- script.js
+++ script.js
@@ -1,19 +1,76 @@
-'use strict';
+'use strict'
 
-// Второе задание
+// третье задание.
 
-// Создать переменную num со значением 266219 (тип данных число)
-let num = 266219;
+//1).Переменная lang может принимать 2 значения: 'ru' 'en'
+//lang === 'ru' || 'en';
+const lang = 'ru';
 
-// Вывести в консоль произведение (умножение) цифр этого числа
-let result = num.toString()
-                .split('')
-                .reduce((mult, currentValue) => mult * +currentValue, 1);
+const daysOfWeekRus = [
+  'Понедельник',
+  'Вторник',
+  'Среда',
+  'Четверг',
+  'Пятница',
+  'Суббота',
+  'Воскресенье'
+];
 
-console.log(result);
+const daysOfWeekEng = [
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+  'Sunday'
+];
 
-//Полученный результат возвести в степень 3, используя только 1 оператор
-result **= 3;
+//через if
+if (lang === 'ru') {
+  console.log(daysOfWeekRus.toString());
+} else if (lang === 'en') {
+  console.log(daysOfWeekEng.toString());
+}
 
-//Вывести в консоль первые 2 цифры полученного числа
-console.log(result.toString().slice(0,2));
+//через switch-case
+switch (lang) {
+  case 'ru':
+    console.log(daysOfWeekRus.toString());
+    break;
+  case 'en':
+    console.log(daysOfWeekEng.toString());
+    break;
+}
+
+/*через многомерный массив без ифов и switch. Тут умнее не придумал.
+И не смог нормально обработать ситуацию, когда такого языка нет.*/
+const arrayDays = [
+  ['ru', daysOfWeekRus], 
+  ['en', daysOfWeekEng]
+];
+let days = arrayDays.filter(language => language[0] === lang)[0][1];
+console.log(days.toString());
+
+//через свойство объекта. Тоже без обработки отсутствия свойства.
+const daysOfWeek = {
+  'ru': daysOfWeekRus,
+  'en': daysOfWeekEng
+};
+console.log(daysOfWeek[lang].toString());
+
+/*
+2). У нас есть переменная namePerson. 
+Если значение этой переменной “Артем” то вывести в консоль “директор”, 
+если значение “Александр” то вывести в консоль “преподаватель”, 
+с любым другим значением вывести в консоль “студент”
+*/
+let namePerson = 'Артем' // 'Артем', 'Александр', 'Иванов Иван Иванович'.
+
+console.log(
+  namePerson === 'Артем'
+               ? 'директор'
+               : namePerson === 'Александр'
+                              ? 'преподаватель'
+                              : 'студент'
+);
