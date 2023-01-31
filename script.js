5359b03b115b76ed7f313963a60d21e854824441
Vladimir Tkachenko <geletar@gmail.com>
2023-01-31 22:29:22 +0400

    Lesson04

--- script.js
+++ script.js
@@ -1,76 +1,17 @@
 'use strict'
 
-// третье задание.
+let userString = '                     Lorem ipsum dolor sit amet. Est architecto soluta sit quasi dolor cum enim saepe et praesentium nesciunt qui magnam esse? Eos enim consequatur vel quis sint hic enim accusamus est dignissimos eveniet?';
+//let userString = '                     Lorem ipsum';
+//let userString = 5;
 
-//1).Переменная lang может принимать 2 значения: 'ru' 'en'
-//lang === 'ru' || 'en';
-const lang = 'ru';
-
-const daysOfWeekRus = [
-  'Понедельник',
-  'Вторник',
-  'Среда',
-  'Четверг',
-  'Пятница',
-  'Суббота',
-  'Воскресенье'
-];
-
-const daysOfWeekEng = [
-  'Monday',
-  'Tuesday',
-  'Wednesday',
-  'Thursday',
-  'Friday',
-  'Saturday',
-  'Sunday'
-];
-
-//через if
-if (lang === 'ru') {
-  console.log(daysOfWeekRus.toString());
-} else if (lang === 'en') {
-  console.log(daysOfWeekEng.toString());
+// четвёртое задание.
+const trimString = (str) => {
+  if (typeof str === 'string') {
+    str = str.trim();
+    alert(str.slice(0, 30) + (str.length > 30 ? '...' : ''));
+  } else {
+    alert('Передана не строка!');
+  }
 }
 
-//через switch-case
-switch (lang) {
-  case 'ru':
-    console.log(daysOfWeekRus.toString());
-    break;
-  case 'en':
-    console.log(daysOfWeekEng.toString());
-    break;
-}
-
-/*через многомерный массив без ифов и switch. Тут умнее не придумал.
-И не смог нормально обработать ситуацию, когда такого языка нет.*/
-const arrayDays = [
-  ['ru', daysOfWeekRus], 
-  ['en', daysOfWeekEng]
-];
-let days = arrayDays.filter(language => language[0] === lang)[0][1];
-console.log(days.toString());
-
-//через свойство объекта. Тоже без обработки отсутствия свойства.
-const daysOfWeek = {
-  'ru': daysOfWeekRus,
-  'en': daysOfWeekEng
-};
-console.log(daysOfWeek[lang].toString());
-
-/*
-2). У нас есть переменная namePerson. 
-Если значение этой переменной “Артем” то вывести в консоль “директор”, 
-если значение “Александр” то вывести в консоль “преподаватель”, 
-с любым другим значением вывести в консоль “студент”
-*/
-let namePerson = 'Артем' // 'Артем', 'Александр', 'Иванов Иван Иванович'.
-
-console.log(
-  namePerson === 'Артем'
-               ? 'директор'
-               : namePerson === 'Александр'
-                              ? 'преподаватель'
-                              : 'студент'
-);
+trimString(userString);
