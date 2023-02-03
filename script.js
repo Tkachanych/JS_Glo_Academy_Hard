d104fff7406968ab0ae2da86c8d3f3872a419563
Vladimir Tkachenko <geletar@gmail.com>
2023-02-03 23:08:19 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,31 @@
 'use strict';
 
+const date = new Date();
+const curDay = date.getDay() === 0 ? 6 : date.getDay() - 1;
+
+const week = [
+  'понедельник',
+  'вторник',
+  'среда',
+  'четверг',
+  'пятница',
+  'суббота',
+  'воскресенье'
+];
+
+for (let i in week) {
+
+  let html = week[i];
+  const div = document.createElement('div');
+
+  if (i > 4) {
+    html = `<i>${html}</i>`
+  };
+
+  if (+i === curDay) {
+    html = `<b>${html}</b>`
+  };
+
+  div.innerHTML = html;
+  document.body.appendChild(div);
+}
