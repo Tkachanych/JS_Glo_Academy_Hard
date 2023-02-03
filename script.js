814df25a1cdfa747ae233a343dff485a7e3cbab9
Vladimir Tkachenko <geletar@gmail.com>
2023-02-03 09:11:45 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 'use strict'
 
 let userString = '                     Lorem ipsum dolor sit amet. Est architecto soluta sit quasi dolor cum enim saepe et praesentium nesciunt qui magnam esse? Eos enim consequatur vel quis sint hic enim accusamus est dignissimos eveniet?';
-//let userString = '                     Lorem ipsum';
-//let userString = 5;
 
 // четвёртое задание.
 const trimString = (str) => {
-  if (typeof str === 'string') {
-    str = str.trim();
-    alert(str.slice(0, 30) + (str.length > 30 ? '...' : ''));
-  } else {
-    alert('Передана не строка!');
-  }
+  if (typeof str !== 'string') throw new TypeError('Передана не строка!');
+
+  str = str.trim();
+
+  return str.slice(0, 30) + (str.length > 30 ? '...' : '');
 }
 
-trimString(userString);
+console.log(trimString(userString));
+console.log(trimString('Привет!'));
+console.log(trimString({wtf: `WTF`}));
