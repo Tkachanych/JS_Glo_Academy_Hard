9257050a976764609a47385f3bf2a8223596ee5d
Vladimir Tkachenko <geletar@gmail.com>
2023-02-08 14:56:59 +0400

    lesson10

--- script.js
+++ script.js
@@ -1,36 +1,12 @@
 'use strict';
 
-// Первое задание
+const button = document.querySelector('button');
+const input = document.querySelector('input');
+const list = document.querySelector('ul');
 
-let title = 'JS_Glo_Academy';
-let screens = 'Простые, Сложные, Интерактивные';
-let screenPrice = 2500;
-let rollback = 15;
-let fullPrice = 500000;
-let adaptive = true;
-
-console.log(typeof title);
-console.log(typeof fullPrice);
-console.log(typeof adaptive);
-console.log(screens.length);
-console.log(`Стоимость верстки экранов ${screenPrice} рублей`);
-console.log(`Стоимость разработки сайта ${fullPrice} рублей`);
-console.log(screens.toLowerCase().split(', '));
-console.log(fullPrice * (rollback / 100));
-
-// Второе задание
-
-// Создать переменную num со значением 266219 (тип данных число)
-let num = 266219;
-
-// Вывести в консоль произведение (умножение) цифр этого числа
-let result = num.toString()
-                .split('')
-                .reduce((sum, currentValue) => sum * +currentValue, 1);
-
-console.log(result);
-
-//Полученный результат возвести в степень 3, используя только 1 оператор
-result **= 3;
-
-console.log(result);
+button.onclick = function(){
+  console.clear();
+  const newElem = list.querySelector('li').cloneNode(true);
+  newElem.innerHTML = input.value;
+  list.append(newElem);
+}
