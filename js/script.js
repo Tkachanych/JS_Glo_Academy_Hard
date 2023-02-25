a6382674de9939c5b562d3b1fa0f56cde4cb5867
Vladimir Tkachenko <geletar@gmail.com>
2023-02-25 22:01:29 +0400

    lesson19_hard

--- js/script.js
+++ js/script.js
@@ -1,2 +1,18 @@
 'use strict';
 
+const input = document.querySelector('input');
+const par = document.querySelector('p');
+
+function debounce(func, timeout = 1000){
+  let timer;
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => { func.apply(this, args); }, timeout);
+  };
+}
+
+function fillPar(){
+  par.textContent = input.value;
+}
+
+input.addEventListener('keyup', debounce(() => fillPar()));
